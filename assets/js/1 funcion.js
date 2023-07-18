2589de8c873bf6d1780831988d1ca5c63e1c715e
Claudia Villarroel <claudia.villarroel@live.com>
2023-07-17 23:07:08 -0400

    Se agregan pruebas de los 2 primeros retos

--- assets/js/1 funcion.js
+++ assets/js/1 funcion.js
@@ -2,4 +2,9 @@
 const example = function (a, b, c) {
   return a + b + c;
 };
-example(1, 2, 3);
+
+/*Pruebas de ejecucion*/
+let resultado = example(1, 2, 3);
+console.log(resultado);
+resultado = example("hola ", "como ", "estas");
+console.log(resultado);
--- assets/js/2 arrow.js
+++ assets/js/2 arrow.js
@@ -1,3 +1,8 @@
 /*Transformar a una arrow fuction en una linea*/
 const suma = (a,b) => (a+b);
-suma(2,3);
+
+/*Pruebas de ejecucion*/
+let resultado = suma(2,3);
+console.log("Resultado de 2 + 3" + resultado);
+resultado = suma("a","b");
+console.log("Resultado de a+b"+resultado)
